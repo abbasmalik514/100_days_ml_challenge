df18f5eaf149aee0704ddf128510edfbab70fb76
abbasmalik514 <42239492+abbasmalik514@users.noreply.github.com>
2020-03-26 10:39:29 +0500

    Update Multiple_Linear_Regresson.py

--- Day_3_Multiple_Linear_Regression/Multiple_Linear_Regresson.py
+++ Day_3_Multiple_Linear_Regression/Multiple_Linear_Regresson.py
@@ -1,7 +1,7 @@
 # Reading data from the file
 import numpy as np
 import pandas as pd
-dataset = pd.read_csv('/content/drive/My Drive/50_Startups.csv')
+dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,4].values
 print(X)
